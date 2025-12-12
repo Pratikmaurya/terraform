ce24f8f3ed119f52be5a58e86300e12831b08f42
pratik maurya <pratikmaurya97@gmail.com>
2025-12-11 20:41:36 -0500

    Added remote backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstate21215"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
